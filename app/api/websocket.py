f9b4d52871984dc7fc587691406f2e03faad37f9
nofancy <noalexfancy@gmail.com>
2024-10-01 15:14:40 +0300

    delete services/websocket.py

--- app/api/websocket.py
+++ app/api/websocket.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, WebSocket, Depends
 from redis.asyncio import Redis
-
-from app.services.websocket import worker
+import asyncio
 from app.services.redis import get_redis
 
 router = APIRouter()
@@ -9,7 +8,36 @@ router = APIRouter()
 
 @router.websocket("/ws/{channel}")
 async def websocket_endpoint(websocket: WebSocket, redis: Redis = Depends(get_redis), channel: str = '*'):
-    await worker(websocket, redis, channel)
+    await websocket.accept()
+
+    ps = redis.pubsub()
+
+    async def listen_socket():
+        while True:
+            try:
+                await websocket.receive_text()
+            except Exception as e:
+                raise Exception(f"Error in listen_socket: {e}")
+
+    async def listen_redis():
+        await ps.psubscribe(channel)
+        try:
+            async for message in ps.listen():
+                if message['type'] == 'pmessage':
+                    await websocket.send_text(message['data'])
+        except Exception as e:
+            raise Exception(f"Error in listen_redis: {e}")
+
+    try:
+        tasks = [listen_socket(), listen_redis()]
+        await asyncio.gather(*tasks)
+    except Exception as e:
+        print(f"Error in websocket_endpoint: {e}")
+    finally:
+        await ps.punsubscribe(channel)
+        await redis.close()
+        await redis.close()
+        print('Connection closed')
 
 
 @router.get('/test/{channel}')
